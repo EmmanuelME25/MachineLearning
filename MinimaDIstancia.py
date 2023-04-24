a03dcc660d1870b6769b79751d74a81766962207
EmmanuelME25 <medina_emmanuel25@hotmail.com>
2023-04-23 23:21:55 -0600

    2.0

--- MinimaDIstancia.py
+++ MinimaDIstancia.py
@@ -1,16 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas
+import math
 
 
 class Patron:
 
-    def __init__(self, vector, clase):
+    def Patron(self, vector, clase):
         self.vector = vector
         self.clase = clase
         self.claseResultante = "Desconocida"
 
-    def __init__(self, n):
+    def Patron(self, n):
         self.vector = [0.0] * n
         self.clase = "Desconocida"
         self.claseResultante = "Desconocida"
@@ -35,18 +36,35 @@ class Patron:
 
 
 class PatronRepresentativo(Patron):
-
     def __init__(self, vector, clase):
         super().__init__(vector, clase)
         self.acumulador = 0
 
+    def __init__(self, n):
+        super().__init__(n)
+        self.acumulador = 0
+
     def acumular(self, aux):
-        # iterate through the characteristic vector to accumulate
-        for i in range(len(super().getVector())):
-            super().getVector()[i] += aux.getVector()[i]
+        for x in range(len(self.getVector())):
+            self.getVector()[x] += aux.getVector()[x]
         self.acumulador += 1
 
     def promediar(self):
-        for i in range(len(super().getVector())):
-            super().getVector()[i] /= self.acumulador
+        for x in range(len(self.getVector())):
+            self.getVector()[x] /= self.acumulador
+
+class Herramientas():
+    @staticmethod
+    def distanciaEuclidiana(a, b):
+        acu = 0
+        for x in range(len(a.getVector())):
+            res = a.getVector()[x] - b.getVector()[x]
+            res *= res
+            acu = acu + res
+        return math.sqrt(acu)
+
+    @staticmethod
+    def calcularDistribucion(media, varianza, c):
+        return (1 / (math.sqrt(math.pi * 2 * varianza))) * (
+            math.exp(-1 * ((math.pow((c - media), 2)) / (2 * varianza))))
 
