5874f0ea3e22e370b8a68a972e8b022dec686b89
EmmanuelME25 <52678688+EmmanuelME25@users.noreply.github.com>
2023-06-01 15:19:09 -0600

    Add files via upload

--- MinimaDIstancia.py
+++ MinimaDIstancia.py
@@ -68,3 +68,66 @@ class Herramientas():
         return (1 / (math.sqrt(math.pi * 2 * varianza))) * (
             math.exp(-1 * ((math.pow((c - media), 2)) / (2 * varianza))))
 
+
+class MinimaDistancia(List[Patron]):
+    def __init__(self):
+        self.medias = None
+        self.eficacia = None
+
+    def entrenar(self, aux: List[Patron]) -> None:
+        # calcular las medias de cada clase
+        self.medias = []
+        # recorrer la lista de entrenamiento
+        for p in aux:
+            # verificar si existe elemento representativo
+            if not self.existeRepresentativo(self.medias, p):
+                # tiene que generar el representativo y acumular
+                self.generarMediaRepresentativa(p, self.medias)
+        # iterar las medias
+        for pr in self.medias:
+            pr.promediar()
+
+    def existeRepresentativo(self, medias: List[PatronRepresentativo], p: Patron) -> bool:
+        # recorrer la medias y verificar si existe alguna con un mismo nombre de clase que "p"
+        for media in medias:
+            if media.getClase() == p.getClase():
+                media.acumular(p)
+                return True
+        return False
+
+    def generarMediaRepresentativa(self, p: Patron, medias: List[PatronRepresentativo]) -> None:
+        # generar por referencia una media representativa
+        aux = PatronRepresentativo([0] * len(p.getVector()), p.getClase())
+        aux.acumular(p)
+        medias.append(aux)
+
+    def clasificar(self, aux: Patron) -> None:
+        # generar una hipotesis
+        i = 0
+        di = Herramientas.distanciaEuclidiana(self.medias[i], aux)
+        for x in range(1, len(self.medias)):
+            j = Herramientas.distanciaEuclidiana(self.medias[x], aux)
+            if j < di:
+                di = j
+                i = x
+        # aux.setClase(self.medias[i].getClase())
+        aux.setClaseResultante(self.medias[i].getClase())
+
+    def clasificarConjunto(self, conjunto: List[Patron]) -> None:
+        # para cada elemento de la coleccion se genera un proceso
+        # de clasificación
+        self.eficacia = 0
+        c = 0
+        for aux in conjunto:
+            self.clasificar(aux)
+            # if(aux.getClase().equals(aux.getClaseResultante()))c++;
+        # calcular la eficacia del clasificador
+        # proceso iterativo de a coleccion "conjunto"
+        # h = (double)100/conjunto.size();
+        # this.eficacia = h*c;
+        # mandar llamar a MC
+
+        print()
+
+    def getEficacia(self) -> float:
+        return self.eficacia
